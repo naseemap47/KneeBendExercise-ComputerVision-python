ab9e585c8e20709cc00815c271ae211e0fb991ed
NASEEM A P <naseemalassampattil@gmail.com>
2022-04-17 13:15:05 +0530

    writen function to find angle between knee

---
+++ find_angle.py
@@ -0,0 +1,18 @@
+import math
+
+
+def get_angle(list_lm, p1, p2, p3):
+    if len(list_lm) > 30:
+        x1, y1 = list_lm[p1][1:]
+        x2, y2 = list_lm[p2][1:]
+        x3, y3 = list_lm[p3][1:]
+
+        # Angle
+        angle = math.degrees(
+            math.atan2(y3 - y2, x3 - x2) -
+            math.atan2(y1 - y2, x1 - x2)
+        )
+
+        return angle
+
+
