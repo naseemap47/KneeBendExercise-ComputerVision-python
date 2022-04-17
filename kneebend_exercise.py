a25dd27849bd7f2d974b20082dbb4c94e911374f
NASEEM A P <naseemalassampattil@gmail.com>
2022-04-17 12:38:20 +0530

    printed pose id and landmarks

--- kneebend_exercise.py
+++ kneebend_exercise.py
@@ -10,7 +10,11 @@ while True:
     success, img = cap.read()
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     result = pose.process(img_rgb)
-    print(result.pose_landmarks)
+    # print(result.pose_landmarks)
+
+    if result.pose_landmarks:
+        for id, lm in enumerate(result.pose_landmarks.landmark):
+            print(id, lm)
 
     cv2.imshow('Video', img)
     cv2.waitKey(1)
