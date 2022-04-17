b9d92a097a20cc84861ab6cbee34880983d1c7e1
NASEEM A P <naseemalassampattil@gmail.com>
2022-04-17 12:35:02 +0530

    detecting properly, getting pose landmarks

--- kneebend_exercise.py
+++ kneebend_exercise.py
@@ -1,9 +1,16 @@
 import cv2
+import mediapipe as mp
+import time
 
 cap = cv2.VideoCapture('KneeBendVideo.mp4')
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose()
 
 while True:
     success, img = cap.read()
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    result = pose.process(img_rgb)
+    print(result.pose_landmarks)
 
     cv2.imshow('Video', img)
     cv2.waitKey(1)
